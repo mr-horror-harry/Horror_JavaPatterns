acba1d9a1069ec2a376dac283e1cbc440685a29d
M A Harish J <103276323+mr-horror-harry@users.noreply.github.com>
2023-03-08 12:37:39 +0530

    Add files via upload

---
+++ X_form_of_given_string.java
@@ -0,0 +1,18 @@
+// Print an X-form of word of odd length
+
+import java.util.*;
+
+public class X_form_of_given_string
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    String s = sc.next();
+	    
+	    for(int i=0; i<s.length(); i++){
+	        for(int j=0; j<s.length(); j++){
+	             System.out.print(i==j || i+j==s.length()-1 ? s.charAt(j): " ");
+	        }
+	        System.out.println();
+	    }
+	}
+}
