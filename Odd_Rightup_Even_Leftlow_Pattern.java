4295486b11f3d6a7ebc0941e9bc21021d42e7b2d
M A Harish J <mr.horror3737@gmail.com>
2023-06-03 15:08:11 +0530

    mar pttn

---
+++ Odd_Rightup_Even_Leftlow_Pattern.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class OddRightupEvenLeftlow {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a Number: ");
+        int n = sc.nextInt();
+        int[][] mt = new int[n][n];
+
+        int o = 1, e = 2;
+
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < n; i++) {
+                if (i < j) {
+                    mt[i][j] = o;
+                    o += 2;
+                } else if (i > j) {
+                    mt[i][j] = e;
+                    e += 2;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                System.out.print(mt[i][j] + " ");
+
+            System.out.println();
+        }
+    }
+}
